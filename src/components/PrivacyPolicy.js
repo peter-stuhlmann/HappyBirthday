59c9c53b68e7326afcce23835743ab65c185a37e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-18 23:11:56 +0200

    Add first part of privacy policy text

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -4,7 +4,45 @@ export default class PrivacyPolicy extends Component {
   render() {
     return (
       <Fragment>
-        <i>Coming soon ...</i>
+        <h1>Datenschutzerklärung</h1>
+        
+        <p>Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz „Daten“) im Rahmen der Erbringung unserer Leistungen sowie innerhalb unseres Onlineangebotes und der mit ihm verbundenen Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unser Social Media Profile auf (nachfolgend gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO). <br />
+        <br />
+        </p>
+        
+        <h3>Verantwortlicher</h3>
+        <p>
+            Peter R. Stuhlmann<br />
+            Martha-Arendsee-Str. 10<br />
+            12681 Berlin<br />
+            <br />
+            Mail: info@peter-stuhlmann.de<br />
+            Mobil: +49 1578 5630944
+        </p>
+
+        <h3>Arten der verarbeiteten Daten</h3>
+        <ul>
+            <li>Bestandsdaten (z.B., Personen-Stammdaten, Namen oder Adressen).</li>
+            <li>Kontaktdaten (z.B., E-Mail, Telefonnummern).</li>
+            <li>Inhaltsdaten (z.B., Texteingaben, Fotografien, Videos).</li>
+            <li>Nutzungsdaten (z.B., besuchte Webseiten, Interesse an Inhalten, Zugriffszeiten).</li>
+            <li>Meta-/Kommunikationsdaten (z.B., Geräte-Informationen, IP-Adressen).</li>
+        </ul>
+        
+        <h3>Kategorien betroffener Personen</h3>
+        <p>Besucher und Nutzer des Onlineangebotes (Nachfolgend bezeichnen wir die betroffenen Personen zusammenfassend auch als „Nutzer“).</p>
+        
+        <h3>Zweck der Verarbeitung</h3>
+        <ul>
+            <li>Zurverfügungstellung des Onlineangebotes, seiner Funktionen und  Inhalte.</li>
+            <li>Beantwortung von Kontaktanfragen und Kommunikation mit Nutzern.</li>
+            <li>Sicherheitsmaßnahmen.</li>
+            <li>Reichweitenmessung/Marketing</li>
+        </ul>
+
+        <p>...</p>
+
+        <i>The rest will coming soon ...</i>
       </Fragment>
     );
   }
