75ac805f16cdae60932036eaafb6e214b2744fde
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-18 12:56:09 +0200

    Add customized 'Happ birthday' message
    The path of url is what is showing as name in the message and title tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,16 @@ import React, { Component, Fragment } from 'react';
 
 export default class Home extends Component {
 
+  name = window.location.pathname.slice(1)
+
   componentDidMount() {
-    document.title = "Happy Birthday!"
+    document.title = `Happy Birthday, ${ this.name }!`
   }
 
   render() {
     return (
       <Fragment>
-
+        <h1>Happy Birthday, { this.name }!</h1>
       </Fragment>
     );
   }
