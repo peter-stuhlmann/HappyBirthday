a5834f57b3c4f2fffe51937a5dc648d7adbe872c
AD <atar.dci@gmail.com>
2019-06-19 14:54:13 +0200

    Add text animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import Balloons from './Balloons';
 export default class Home extends Component {
   state = {
     receiver: window.location.pathname.slice(1) || undefined,
-    sender: window.location.hash.slice(1).replace(/%20/gi, " ")
+    sender: window.location.hash.slice(1).replace(/%20/gi, " "),
+    renderText: false
   }
 
   componentDidMount() {
@@ -14,16 +15,21 @@ export default class Home extends Component {
     this.meta.name = 'description';
     this.meta.setAttribute('content', 'Happy birthday! Send personalized birthday greetings to all your friends - for free and without registration!');
     document.getElementsByTagName('head')[0].appendChild(this.meta);
+
+    setTimeout(
+      () => { this.setState({ renderText: true }) }
+      , 8500
+    )
   }
 
   render() {
     return (
       <Fragment>
         <Balloons />
-        <main>
+        {this.state.renderText && <main>
           <p className="receiver">Happy birthday{this.state.receiver && ", " + this.state.receiver}!</p>
           <p className="sender">{this.state.sender}</p>
-        </main>
+        </main>}
       </Fragment>
     );
   }
